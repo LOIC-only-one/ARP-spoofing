7eb7892f0c9ef3d75c4aa514d1b7e280b32ec939
Maurer <archim141@gmail.com>
2023-12-17 15:46:18 +0100

    Push Main

--- functions.py
+++ functions.py
@@ -2,25 +2,34 @@ from scapy.all import sr1, IP, ICMP, Ether, ARP
 from sys import *
 
 def decouverte_active(host):
-    response = sr1(IP(dst=host) / ICMP(), timeout=3, iface=None)
-    if response:
-        print(f"L'hôte {host} est joignable !")
-    else:
-        print(f"L'hôte {host} est injoignable !")
+    try:
+        response = sr1(IP(dst=host) / ICMP(), timeout=3, iface=None)
+        if response:
+            return f"L'hote {host} est joignable !"
+        else:
+            return f"L'hote {host} est injoignable !"
+    except Exception as erreur:
+        return f"Erreur lors de la découverte de l'hote {host}: {str(erreur)}"
 
 def decouverte_passive(host):
-    response = sr1(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=host), timeout=3, iface=None)
-    if response:
-        print(f"L'hôte {host} est joignable !")
-    else:
-        print(f"L'hôte {host} est injoignable !")
-        
+    try:
+        response = sr1(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=host), timeout=3, iface=None)
+        if response:
+            return f"L'hote {host} est joignable !"
+        else:
+            return f"L'hote {host} est injoignable !"
+    except Exception as erreur:
+        return f"Erreur lors de la découverte de l'hote {host}: {str(erreur)}"
 
 def decouverte_reseau(network, mask):
-    range = f'{network}/{mask}'
+    try:
+        range = f'{network}/{mask}'
+        # Explorer tous les hotes dans la plage spécifiée
+        # Faire une boucle pour tous decouvrir
+    except Exception as erreur:
+        return f"Erreur lors de la découverte du réseau {network}/{mask}: {str(erreur)}"
 
 def exporter_resultat(data, fichier):
     with open(fichier, 'w') as file:
         file.write(data + '\n')
-
-decouverte_passive('127.0.0.1')
+        file.write("###########################################")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-from functions import *
+from functions import decouverte_active, decouverte_passive, decouverte_reseau, exporter_resultat
+
 def main():
     parser = argparse.ArgumentParser(description="Script de Découverte d'Hôtes")
     parser.add_argument("-a", "--actif", metavar="ADRESSE_IP_CIBLE", help="Effectuer une découverte active pour l'adresse IP cible.")
@@ -8,18 +9,23 @@ def main():
     parser.add_argument("-x", "--exporter", metavar="FICHIER_SORTIE", help="Exporter le résultat de la découverte vers le fichier spécifié.")
 
     args = parser.parse_args()
+    result = ""
 
     if args.actif:
-        decouverte_active(args.actif)
+        result = decouverte_active(args.actif)
+        print(result)
     elif args.passif:
-        decouverte_passive(args.passif)
+        result = decouverte_passive(args.passif)
+        print(result)
     elif args.reseau:
-        decouverte_reseau(args.reseau)
+        result = decouverte_reseau(args.reseau)
+        print(result)
     else:
         print("Veuillez spécifier une action : -a, -p, ou -t.")
 
     if args.exporter:
-        exporter_resultat("Résultat d'exemple", args.exporter)
+        exporter_resultat(result, args.exporter)
+        print(f"Résultat exporté vers {args.exporter}.")
 
 if __name__ == "__main__":
     main()
