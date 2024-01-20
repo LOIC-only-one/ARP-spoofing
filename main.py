bce8db8e21cf4882b30aee944b097ae4e774ff42
BeardDemon <75929039+LOIC-only-one@users.noreply.github.com>
2024-01-20 17:32:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,147 +1,100 @@
-from scapy.all import sr1, IP, ICMP, Ether, ARP, sniff
+##########Importation##############
+from scapy.all import sr1, IP, ICMP, ARP, sniff, sr
 import ipaddress
 import argparse
+from colorama import Fore, Style
 
-class Colors:
-    """ ANSI color codes """
-    RED = "\033[0;31m"
-    GREEN = "\033[0;32m"
-    BLUE = "\033[0;34m"
-    YELLOW = "\033[1;33m"
-    END = "\033[0m"
+##########Constente##########
+NOM_IFACE = None
 
 #########Fonction Parser##########
 def main():
-    parser = argparse.ArgumentParser(description="Script de Découverte d'Hôtes")
+    parser = argparse.ArgumentParser(description="Script de Découverte d'hotes")
     parser.add_argument("-a", "--actif", metavar="ADRESSE_IP_CIBLE", help="Effectuer une découverte active pour l'adresse IP cible.")
     parser.add_argument("-p", "--passif", metavar="ADRESSE_IP_CIBLE", help="Effectuer une découverte passive pour l'adresse IP cible.")
     parser.add_argument("-t", "--reseau", metavar="RESEAU", help="Effectuer une découverte du réseau en utilisant ICMP pour le réseau spécifié.")
     parser.add_argument("-x", "--exporter", metavar="FICHIER_SORTIE", help="Exporter le résultat de la découverte vers le fichier spécifié.")
-    parser.add_argument("-c", "--clear", help="Supprimer le contenu du fichier spécifié.")
-
+    
     args = parser.parse_args()
-    result = ""
-
     if args.actif:
         result = decouverte_active(args.actif)
         print(result)
+    if args.exporter:
+        exporter_resultat(result, args.exporter)
+        print(f"{Fore.YELLOW}Résultat exporté vers {args.exporter}.{Style.RESET_ALL}")
     elif args.passif:
         result = decouverte_passive(args.passif)
         print(result)
     elif args.reseau:
         result = decouverte_reseau(args.reseau)
         print(result)
-    
-    elif args.clear:
-        clear_log(args.clear)
-        print(f"Contenu de {args.clear} supprimé.")
-        
     else:
-        print("Veuillez spécifier une action : -a, -p, ou -t.")
+        print("Veuillez spécifier une action : -a, -p, ou -t ou -x/-c")
 
-    if args.exporter:
-        exporter_resultat(result, args.exporter)
-        print(f"{Colors.YELLOW}Résultat exporté vers {args.exporter}.{Colors.END}")
-
-####### Network Functions #######
+####### Network Functions ########
 def decouverte_active(host, timeout=4, iface=None):
     """
-    Objectif : Découvrir un hôte occupant une adresse IP sur un réseau
-    @host --> <adresse_ipV4>
-    @timeout --> <timer_entre_chaque_envoi>
-    @iface --> <interface_listen>
+    Objectif : Découvrir un hote occupant une adresse IP sur un réseau
     """
     response = sr1(IP(dst=host) / ICMP(), timeout=timeout, iface=iface)
+    if response and response.haslayer(ICMP) and response[ICMP].type == 0:
+        print(f"{Fore.GREEN}[+] L'hote {host} est joignable !{Style.RESET_ALL}")
+        return f"{Fore.GREEN}[+] L'hote {host} est joignable !{Style.RESET_ALL}"
+    else:
+        print(f"{Fore.RED}[+] L'hote {host} ne réponds pas à ICMP !{Style.RESET_ALL}")
+        return None ##traitement de la valeur None dans @decouverte_reseau
 
-    try:
-        if response is not None and response[ICMP].type == 0:
-            return (f"{Colors.GREEN}L'hôte {host} est joignable !{Colors.END}")
-        else:
-            print(f"{Colors.RED}L'hôte {host} est injoignable !{Colors.END}")
-            pass
-    except Exception as erreur:
-        return (f"{Colors.YELLOW}Une erreur est survenue, {host} injoignable ...., {str(erreur)}{Colors.END}")
-
-
-def decouverte_passive(target_ip, timeout=35, iface="Wi-Fi"):
+def decouverte_passive(ip_cible, timeout=35):
     """
-    Objectif : Sniffer les paquets ARP pour détecter la joignabilité de l'hôte sur le réseau
-    @target_ip --> <adresse_ipv4>
-    @timeout --> <temps_de_capture>
-    @iface --> <interface_listen>
+    Objectif : Sniffer les paquets ARP pour détecter la joignabilité de l'hote sur le réseau
     """
-    host_reachable = False  # Variable pour indiquer si l'hôte est détecté
+    host_reachable = False
+    def arp_filtre(packet):
+        nonlocal host_reachable
+        if packet.haslayer("ARP"):
+            if packet[ARP].op == 1 or packet[ARP].op == 2: # 1 = requête, 2 = réponse
+                if packet[ARP].psrc == ip_cible or packet[ARP].pdst == ip_cible:
+                    print(f"[+] {Fore.GREEN}{ip_cible} | interaction ARP (Adresse MAC: {packet[ARP].hwsrc}{Style.RESET_ALL})")
+                    host_reachable = True
 
-    def arp_callback(packet,host_reachable=host_reachable):
-
-        if packet.haslayer(ARP) and packet[ARP].op in [1, 2]:  # Vérification de la couche ARP
-            if packet[ARP].psrc == target_ip:
-                print(f"{target_ip} a répondu à l'ARP (Adresse MAC: {packet[ARP].hwsrc})")
-                host_reachable = True  # L'hôte a été détecté
-
-    print(f"Sniffing ARP pour {timeout} secondes. Appuyez sur Ctrl+C pour arrêter.")
-    sniff(timeout=timeout, iface=iface, prn=arp_callback)
+    print(f"{Fore.YELLOW}Sniffing ARP pour {timeout} secondes. Appuyez sur Ctrl+C pour arrêter.{Style.RESET_ALL}")
+    sniff(timeout=timeout, iface=None, prn=arp_filtre)
 
     if host_reachable:
-        print(f"L'hôte {target_ip} est joignable.")
+        return f"{Fore.GREEN}L'hote {ip_cible} est joignable.{Style.RESET_ALL}"
     else:
-        print(f"L'hôte {target_ip} est injoignable.")
-
-"""
-Alternative à la fonction decouverte_passive
-
-def decouverte_passive(host, timeout=4, iface="Wi-Fi"):
-    Objectif : Réaliser la découverte d'un hôte spécifique à l'aide de ARP
-    @host --> <adresse_ipv4>
-    @timeout --> <temps_entre_differents_tests>
-    @iface --> <interface_listen>
-    try:
-        if iface is not None:
-            response = sr1(ARP(pdst=host), timeout=timeout, iface=iface)
-        else:
-            response = sr1(ARP(pdst=host), timeout=timeout)
-
-        if response and response.haslayer(ARP) and response[ARP].op in [1, 2]:  # Vérification de la couche ARP
-            return f"L'hôte {host} est joignable ! (Adresse MAC: {response[ARP].hwsrc})"
-        else:
-            return f"L'hôte {host} est injoignable !"
-    except Exception as erreur:
-        return f"Erreur inattendue lors de la découverte de l'hôte {host}: {str(erreur)}"
-"""
+        return f"{Fore.RED}L'hote {ip_cible} est injoignable.{Style.RESET_ALL}"
 
 def decouverte_reseau(network):
     """
     Objectif : Découvrir l'intégralité d'un réseau à l'aide de ICMP soit func/decouverte_active
-    @network --> <reseau_a_decouvrir/mask>
     """
+    ##Recuperation des hosts
     results = []
-    network_obj = ipaddress.IPv4Network(f'{network}', strict=False).hosts()
-    for ip in network_obj:
-        results.append(decouverte_active(str(ip), timeout=4))
+    host = ipaddress.IPv4Network(f'{network}', strict=False).hosts()
+    for ip in host:
+        result = decouverte_active(str(ip), timeout=2)
+        if result is not None:  # Ajouter cette condition pour éviter d'ajouter les valeurs None à la liste
+            results.append(result)
 
-    ##Traitement de la liste
-    results = list(set(results))
-    results.remove(None)
-    return results
+    ## Traitement de la liste pour supprimer les doublons
+    unique_results = []
+    for element in results:
+        if element not in unique_results:
+            unique_results.append(element)
+    for element in unique_results:
+        print(element)
 
+    return unique_results
+
+#########Fonction Exporter##########
 def exporter_resultat(data, fichier):
     """
     Objectif : Sortir l'intégralité des datas si demandé d'une fonction
-    @data --> <entree_a_ecrire>
-    @fichier --> <fichier_src>
     """
     with open(fichier, 'a') as file:
         file.write(''.join(data) + '\n')
         file.write("###########################################" + '\n')
-        file.close()
-
-def clear_log(fichier):
-    """
-    Objectif : Supprimer l'entree du fichier
-    @fichier --> <fichier_src>
-    """
-    with open(fichier, 'w') as file:
-        file.write("")
         file.close()
         
 if __name__ == "__main__":
